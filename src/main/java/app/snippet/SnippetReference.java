bb98c2df829a61afc4bb75513e5814b233d60d17
gijswijnholds <g.wijnholds@sig.eu>
2016-08-02 13:47:45 +0200

    worked on the SnippetController

--- src/main/java/app/snippet/SnippetReference.java
+++ src/main/java/app/snippet/SnippetReference.java
@@ -1,15 +1,19 @@
 package app.snippet;
 
+import app.util.PathUtils;
+
 public class SnippetReference {
 
     private final String language;
     private final int chapter;
     private final String path;
+    private final String refName;
 
     public SnippetReference(String language, int chapter, String path) {
         this.language = language;
         this.chapter = chapter;
         this.path = path;
+        this.refName = PathUtils.createNameFromPath(path);
     }
 
     public String getLanguage() {
@@ -28,6 +32,9 @@ public class SnippetReference {
         return language + " in chapter " + chapter + " at path: " + path;
     }
 
+    public String getRefName() {
+        return refName;
+    }
     public String getFileName() {
         String[] fileDir = path.split("/");
         String fileName = fileDir[fileDir.length - 1];
