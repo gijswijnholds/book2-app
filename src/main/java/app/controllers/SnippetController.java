abd47b3cbb0f54e015789489d7999097c4eadea4
gijswijnholds <g.wijnholds@sig.eu>
2016-08-02 16:50:10 +0200

    update on SnippetController to show snippets and urls

--- src/main/java/app/controllers/SnippetController.java
+++ src/main/java/app/controllers/SnippetController.java
@@ -1,14 +1,20 @@
 package app.controllers;
 
 import static app.Application.snippetUrlDao;
+import static app.util.GitHubConstants.CSHARP_LANG;
+import static app.util.GitHubConstants.JAVA_LANG;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang.StringUtils;
+
+import app.snippet.Snippet;
 import app.snippet.SnippetReference;
 import app.util.Path;
+import app.util.PathUtils;
 import app.util.ViewUtil;
 import spark.Request;
 import spark.Response;
@@ -22,9 +28,14 @@ public class SnippetController {
         String language = request.params(":language");
         int chapter = Integer.parseInt(request.params(":chapter"));
         String name = request.params(":name");
+        SnippetReference ref = new SnippetReference(language, chapter,
+            PathUtils.createPathFromName(name, chapter, language));
+        Snippet snippet = snippetUrlDao.getSnippet(ref);
         model.put("language", language);
+        model.put("language-display", StringUtils.capitalize(language));
         model.put("chapter", chapter);
         model.put("name", name);
+        model.put("code", snippet.getCode());
         //    SnippetReference ref = new SnippetReference();
         //  snippetUrlDao.getSnippet(snippetRef);
         return ViewUtil.render(model, Path.Template.ONE_SNIPPET);
@@ -34,6 +45,10 @@ public class SnippetController {
 
         Map<String, Object> model = new HashMap<>();
         String language = request.params(":language");
+        if (!(language.equals(JAVA_LANG) || language.equals(CSHARP_LANG))) {
+            return ViewUtil.notFound;
+        }
+
         List<SnippetReference> snippetRefs = snippetUrlDao.getSnippetRefsByLang(language);
 
         Map<Integer, List<Map<String, String>>> snippetNamesGrouped = snippetRefs.stream()
@@ -50,6 +65,8 @@ public class SnippetController {
         Map<String, String> map = new HashMap<String, String>();
         map.put("name", ref.getFileName());
         map.put("path", ref.getPath());
+        String url = ref.getLanguage() + "/" + ref.getChapter() + "/" + ref.getRefName();
+        map.put("url", url);
         return map;
     }
 
