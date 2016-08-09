b0a94308ac54f901c5d9f3dae19cb6f6d8945b0f
gijswijnholds <g.wijnholds@sig.eu>
2016-08-09 13:23:18 +0200

    unit test for SnippetReference, refactored SnippetReference

--- src/main/java/app/snippet/SnippetReference.java
+++ src/main/java/app/snippet/SnippetReference.java
@@ -7,13 +7,11 @@ public class SnippetReference {
     private final String language;
     private final int chapter;
     private final String path;
-    private final String refName;
 
     public SnippetReference(String language, int chapter, String path) {
         this.language = language;
         this.chapter = chapter;
         this.path = path;
-        this.refName = PathUtils.createNameFromPath(path);
     }
 
     public String getLanguage() {
@@ -33,8 +31,9 @@ public class SnippetReference {
     }
 
     public String getRefName() {
-        return refName;
+        return PathUtils.createNameFromPath(path);
     }
+
     public String getFileName() {
         String[] fileDir = path.split("/");
         String fileName = fileDir[fileDir.length - 1];
---
+++ src/test/java/app/util/SnippetReferenceUtilsTest.java
@@ -0,0 +1,14 @@
+package app.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnippetReferenceUtilsTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
